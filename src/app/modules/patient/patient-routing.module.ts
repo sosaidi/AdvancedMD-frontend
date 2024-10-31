e39e134fa26d8615711eb317814198e4657a1ce0
Soundos Saidi <sosaidi@edu.aau.at>
2024-10-31 13:50:34 +0100

    update because of name change

--- src/app/modules/patient/patient-routing.module.ts
+++ src/app/modules/patient/patient-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core'
 import { RouterModule, Routes } from '@angular/router'
 import { AppointmentsComponent } from './components/appointments/appointments.component'
-import { DashboardComponent } from './components/dashboard/dashboard.component'
+import { PatientDashboardComponent } from './components/dashboard/dashboard.component'
 import { BillingComponent } from './components/billing/billing.component'
 import { MedicalRecordsComponent } from './components/medical-records/medical-records.component'
 import { PrescriptionsComponent } from './components/prescriptions/prescriptions.component'
@@ -9,7 +9,7 @@ import { SettingsComponent } from './components/settings/settings.component'
 
 const routes: Routes = [
   { path: 'appointments', component: AppointmentsComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  { path: 'dashboard', component: PatientDashboardComponent },
   { path: 'billing', component: BillingComponent },
   { path: 'medicalrecords', component: MedicalRecordsComponent },
   { path: 'prescriptions', component: PrescriptionsComponent },
