9e547e977eefc70e9a6aca8d79c392300057d64b
Soundos Saidi <sosaidi@edu.aau.at>
2024-10-31 13:42:57 +0100

    change name to prevent confusion

--- src/app/modules/doctor/components/dashboard/dashboard.component.ts
+++ src/app/modules/doctor/components/dashboard/dashboard.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core'
 
 @Component({
-  selector: 'app-dashboard',
+  selector: 'app-doctor-dashboard',
   standalone: true,
   imports: [],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
 })
-export class DashboardComponent {}
+export class DoctorDashboardComponent {}
