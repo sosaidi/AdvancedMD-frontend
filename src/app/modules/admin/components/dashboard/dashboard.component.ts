e014157f45ef0d541dd552d4bf6a544ae1cf21c2
Pranusha23 <119940119+Pranusha23@users.noreply.github.com>
2024-12-09 13:31:02 +0100

    Copy admin module and components from main to 1-login-tbu branch

---
+++ src/app/modules/admin/components/dashboard/dashboard.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core'
+
+@Component({
+  selector: 'app-admin-dashboard',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css',
+})
+export class AdminDashboardComponent {}
