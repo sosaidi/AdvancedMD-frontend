cc26081c2c22759971b06ae620ae88023eb2d69e
Pranusha23 <119940119+Pranusha23@users.noreply.github.com>
2024-12-09 14:21:05 +0100

    App component added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,35 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterModule, NavigationEnd } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'hospital-management';
-}
+  showNavbar = false;
+  title = 'Advanced Medical Data';
+
+  hiddenRoutes: string[] = ['/login', '/admin/login'];
+
+  constructor(private router: Router) {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        console.log('Navigated to:', event.url);
+        console.log('Show Navbar:' , this.showNavbar);
+
+        // Exclude '/admin/login' from showing the navbar
+        if (event.url === '/admin/login' || event.url === '/login') {
+          this.showNavbar = false;
+        } else {
+          // Show navbar for other '/admin' routes
+          this.showNavbar = event.url.startsWith('/admin');
+        }
+      }
+      });
+    }
+  }
+
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  
+    {path: 'login', loadComponent: () => import('./login/login.component').then(m => m.LoginComponent)},
+    {path: 'admin',  // For routing to Admin module
+    loadChildren: () => import('./modules/admin/admin-routing.module').then(m => m.AdminRoutingModule),
+  },
+  {
+    path: '', redirectTo: '/login', pathMatch: 'full',  
+  },
+  {
+    path: '**', redirectTo: '/login'  
+  }
+];
