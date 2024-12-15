92fb1962f0c9025d52a830f8ad7aa0b88533ab35
Soundos Saidi <sosaidi@edu.aau.at>
2024-12-15 12:45:44 +0100

    copy from doctor view

---
+++ src/app/modules/patient/components/topbar/topbar.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-topbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './topbar.component.html',
+})
+export class TopbarComponent {
+  logout() {
+    // logout logic
+    console.log('User logged out');
+    // redirect to the login page
+  }
+}
+
