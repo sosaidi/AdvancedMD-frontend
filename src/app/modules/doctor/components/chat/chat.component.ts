1cdc0b75f005aedd879a538ee869df49aa7f2403
Soundos Saidi <sosaidi@edu.aau.at>
2024-12-27 18:02:27 +0100

    will be merged later

--- src/app/modules/doctor/components/chat/chat.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core'
-
-@Component({
-  selector: 'app-chat',
-  imports: [],
-  templateUrl: './chat.component.html',
-  styleUrl: './chat.component.css',
-})
-export class ChatComponent {}
