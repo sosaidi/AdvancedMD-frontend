25db85f2bdc6343fae4086e60540e8774015cf61
Pranusha23 <119940119+Pranusha23@users.noreply.github.com>
2025-01-01 12:29:33 +0100

    Added app component structure for Admin module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,47 +1,13 @@
 import { Component } from '@angular/core';
-import { Router, RouterModule, NavigationEnd } from '@angular/router';
-import { CommonModule } from '@angular/common';
-import { DashboardService } from './services/dashboard.service';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [RouterOutlet],
 })
 export class AppComponent {
-  showNavbar = true;
   title = 'Advanced Medical Data';
-
-  // property to hold dashboard data
-  DashboardData = {
-    totalPatients: 0,
-    totalStaff: 0,
-    availableRooms: 0,
-  };
-
-  hiddenRoutes: string[] = ['/login', '/admin/login'];
-
-  constructor(private router: Router, private dashboardService: DashboardService) {
-    this.router.events.subscribe((event) => {
-      if (event instanceof NavigationEnd) {
-        console.log('Navigated to:', event.url);
-        console.log('Show Navbar:', this.showNavbar);
-
-        
-        if (event.url === '/admin/login' || event.url === '/login') {
-          this.showNavbar = false;
-        } else {
-          // Show navbar for other '/admin' routes
-          this.showNavbar = event.url.startsWith('/admin');
-        }
-      }
-    });
-
-    // Fetch dashboard data only if the current user is on the admin dashboard
-    this.dashboardService.getDashboardData().subscribe((data) => {
-      this.DashboardData = data;
-    });
-  }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './modules/admin/components/dashboard/dashboard.component';
-import { AppointmentsComponent } from './modules/admin/components/appointments/appointment.component';
-import { PatientComponent } from './modules/admin/components/patients/patients.component';
 
 export const routes: Routes = [
-  { path: 'dashboard', component: DashboardComponent },
-  { path: 'appointments', component: AppointmentsComponent },
-  { path: 'patients', component: PatientComponent },
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./modules/admin/admin.module').then((m) => m.AdminModule),
+  },
+  { path: '', redirectTo: '/admin', pathMatch: 'full' },
+  { path: '**', redirectTo: '/admin' },
 ];
