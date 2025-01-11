ab67e176f52aaa9c76c3dd4a2d2d8afe7e97bf98
Pranusha23 <119940119+Pranusha23@users.noreply.github.com>
2025-01-11 22:15:33 +0100

    Updated Rooms Component

--- src/app/modules/admin/components/rooms/rooms.component.ts
+++ src/app/modules/admin/components/rooms/rooms.component.ts
@@ -1,11 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AdminRoomsService } from '../../services/rooms.service';
+import { Room } from './rooms.model';
+import { NgIf, NgFor, CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-rooms',
-  imports: [],
   templateUrl: './rooms.component.html',
-  styleUrl: './rooms.component.css'
+  standalone: true,
+  imports: [NgIf, FormsModule, NgFor, CommonModule]
 })
-export class RoomsComponent {
+export class RoomsComponent implements OnInit {
+  currentTab: string = 'roomsList'; 
+  rooms: Room[] = [];  
+  showForm: boolean = false; 
+  roomAdded: boolean = false; 
+  addRoomError: boolean = false;
+  newRoom: Room = { roomId: '', type: '', capacity: 0, status: 'Available' };
+  loading: boolean = false;
 
+  constructor(private roomsService: AdminRoomsService) {}
+
+  ngOnInit(): void {
+    this.loadRooms();  
+  }
+
+  loadRooms(): void {
+    
+    this.roomsService.getRooms().subscribe(
+      (rooms: Room[]) => {
+        this.rooms = rooms;  
+      },
+      (error) => {
+        console.error('Error fetching rooms:', error);  
+      }
+    );
+  }
+
+  openForm(): void {
+    this.showForm = true;
+    this.newRoom = { roomId: '', type: '', capacity: 0, status: 'Available' };  
+  }
+
+  closeForm(): void {
+    this.showForm = false;  
+  }
+
+  addRoom(): void {
+    if (!this.newRoom.type || this.newRoom.capacity <= 0) {
+      return;  
+    }
+
+    this.loading = true;
+
+    // Add the room through the service and handle success or failure
+    this.roomsService.addRoom(this.newRoom);
+    this.rooms.push(this.newRoom);  
+
+    this.closeForm();
+    this.roomAdded = true;  
+    setTimeout(() => this.roomAdded = false, 3000); 
+    this.loading = false;
+  }
+
+  editRoom(room: Room): void {
+    console.log('Editing room:', room);
+  }
+
+  deleteRoom(roomId: string): void {
+    this.roomsService.deleteRoom(roomId);
+    this.rooms = this.rooms.filter((room) => room.roomId !== roomId);  
+  }
+
+  setActiveTab(tab: string): void {
+    this.currentTab = tab;  
+  }
 }
