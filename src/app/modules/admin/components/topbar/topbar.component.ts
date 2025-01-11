a2fe04a6dedc3eb3aba4e20ca32a3fa923f013b1
Pranusha23 <119940119+Pranusha23@users.noreply.github.com>
2025-01-11 22:21:24 +0100

    updated topbar

--- src/app/modules/admin/components/topbar/topbar.component.ts
+++ src/app/modules/admin/components/topbar/topbar.component.ts
@@ -1,11 +1,19 @@
-import { Component } from '@angular/core';
+import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-topbar',
   imports: [],
+  standalone: true,
   templateUrl: './topbar.component.html',
-  styleUrl: './topbar.component.css'
 })
 export class TopbarComponent {
-
+  logout() {
+    // Clear session storage 
+    localStorage.removeItem('authToken');
+    sessionStorage.clear();
+  
+    // Redirect to login page
+    window.location.href = '/login';
+  }
+  
 }
