56bf608b4c8a00c9d5c3d09d81adfe322af99fbc
Soundos Saidi <sosaidi@edu.aau.at>
2025-01-19 20:51:36 +0100

    added the same think to the patient

--- src/app/modules/patient/components/sidebar/sidebar.component.ts
+++ src/app/modules/patient/components/sidebar/sidebar.component.ts
@@ -1,11 +1,11 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
 import { RouterLink, RouterLinkActive } from '@angular/router'
-import { NgIf, NgOptimizedImage } from '@angular/common'
+import { NgIf, NgOptimizedImage, NgStyle } from '@angular/common'
 import { NameService } from '../../services/name.service'
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink, NgIf, RouterLinkActive, NgOptimizedImage],
+  imports: [RouterLink, NgIf, RouterLinkActive, NgOptimizedImage, NgStyle],
   standalone: true,
   templateUrl: './sidebar.component.html',
 })
@@ -13,8 +13,11 @@ export class SidebarComponent implements OnInit {
   @Input() collapsed = false;
   @Output() toggle = new EventEmitter<void>();
 
-  firstName = 'John'
-  lastName = 'Doe'
+  profile = {
+    firstName: 'John',
+    lastName: 'Doe',
+    profilePicture: '', // If empty, initials will be used
+  };
 
   constructor(private nameService: NameService) {}
 
@@ -24,8 +27,20 @@ export class SidebarComponent implements OnInit {
 
   ngOnInit() {
     this.nameService.profile$.subscribe((profile) => {
-      this.firstName = profile.firstName
-      this.lastName = profile.lastName
-    })
+      this.profile = profile; // Includes profilePicture
+    });
+  }
+
+  getInitials(): string {
+    const { firstName, lastName } = this.profile;
+    return (firstName[0] || '') + (lastName[0] || '');
+  }
+
+  generatePastelColor(): string {
+    const hash = Array.from(this.getInitials())
+      .map((char) => char.charCodeAt(0))
+      .reduce((sum, charCode) => sum + charCode, 0);
+    const hue = hash % 360;
+    return `hsl(${hue}, 70%, 80%)`;
   }
 }
