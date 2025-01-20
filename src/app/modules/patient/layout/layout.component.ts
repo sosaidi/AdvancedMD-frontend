aca62cb0b66fe4c38460268e6c4e85bc26d29699
Soundos Saidi <sosaidi@edu.aau.at>
2025-01-20 14:13:45 +0100

    fixed issue with width and height

--- src/app/modules/patient/layout/layout.component.ts
+++ src/app/modules/patient/layout/layout.component.ts
@@ -3,10 +3,11 @@ import { TopbarComponent } from '../components/topbar/topbar.component'
 import { SidebarComponent } from '../components/sidebar/sidebar.component'
 import { RouterOutlet } from '@angular/router'
 import { FooterComponent } from '../../doctor/components/footer/footer.component'
+import { NgClass, NgStyle } from '@angular/common'
 
 @Component({
   selector: 'app-layout',
-  imports: [TopbarComponent, SidebarComponent, RouterOutlet, FooterComponent],
+  imports: [TopbarComponent, SidebarComponent, RouterOutlet, FooterComponent, NgClass, NgStyle, SidebarComponent, TopbarComponent],
   standalone: true,
   templateUrl: './layout.component.html',
 })
